e9264092bfb9cb7a835b251b31b41bbf4b413ff2
arjsabbir88 <arjsabbir88@gmail.com>
2026-01-07 12:12:53 +0600

    trying deploy

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const port = Number(process.env.PORT) || 5000
 async function StartServer() {
   await connectDB();
 
-  app.listen(port);
+  // app.listen(port);
 
   // app.listen(port,()=>{
   //   console.log(`Server running on port ${port}`)
